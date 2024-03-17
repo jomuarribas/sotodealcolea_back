d21febc0d941c22c4edc79397fe7f7642047fd44
jmarribas <jomuarribas@gmail.com>
2024-03-17 07:51:30 +0100

    vercel.json

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const cloudinary = require("cloudinary").v2;
 const cors = require("cors");
-const { sotoDb } = require("./src/config/sotoDb");
 const usersRoutes = require("./src/api/routes/user");
 const residentialsRoutes = require("./src/api/routes/residential");
 const incidentsRoutes = require("./src/api/routes/incident");
 const expensesRoutes = require("./src/api/routes/expense");
+const { sotoDb } = require("./src/config/sotoDb");
 
 const app = express();
 
